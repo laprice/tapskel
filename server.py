cd3fa20eed1a46d31d37c164aa5403a1eae1f447
Larry Price <laprice@gmail.com>
2012-12-11 12:40:53 -0800

    set with logging and more static file types.

--- server.py
+++ server.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
-from tornado import web, ioloop
+from tornado import web, ioloop, options
+import logging
 import json
 import momoko
 
 class IndexHandler(web.RequestHandler):
     def get(self):
-        return self.render('templates/index.html')
+        return self.render('index.html')
+
+
+def better_logging(handler):
+    log_method = logging.info
+    request_time = 1000.0 * handler.request.request_time()
+    log_method("%s %s %.2fms", handler.get_status(),
+               handler._request_summary(), request_time)
 
 
 if __name__=='__main__':
+    import os
     app = web.Application(
         [
             (r'/', IndexHandler),
@@ -17,6 +26,9 @@ if __name__=='__main__':
             (r'/css/(.*)', web.StaticFileHandler, { "path": "./css/" }),
             (r'/img/(.*)', web.StaticFileHandler, { "path": "./img/" })
             ],
-        debug=True)
+        debug=True,
+        template_path=os.path.join(os.getcwd(),'templates'),
+        log_function=better_logging)
+    options.parse_command_line()
     app.listen(1337)
     ioloop.IOLoop.instance().start()
